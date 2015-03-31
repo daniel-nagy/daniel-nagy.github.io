c1268e3e9fb68e956f43bdf506e4df01c58c1052
Daniel Nagy <Danny@Dannys-MacBook-Pro.local>
2015-03-31 12:45:25 -0400

    ripple now works within a scroll element on iPhone

--- app/scripts/directives/ripple.js
+++ app/scripts/directives/ripple.js
@@ -9,9 +9,8 @@ angular.module('me').directive('ripple', ['$animate', function ($animate) {
       });
       
       var style = window.getComputedStyle(element.first());
-      var eventName = element.first().hasOwnProperty('ontouchstart') ? 'touchstart' : 'mousedown';
       
-      element.on(eventName, function (event) {
+      function getInk() {
         var ink = angular.element('<ink></ink>');
         var rect = element.first().getBoundingClientRect();
         var diameter = Math.max(rect.height, rect.width);
@@ -24,13 +23,56 @@ angular.module('me').directive('ripple', ['$animate', function ($animate) {
           'background-color': attrs.inkColor || style.color
         });
         
+        return ink;
+      }
+      
+      function showInk(ink) {
         $animate.enter(ink, element).then(function () {
           ink.remove();
         }).finally(scope.$apply());
-      });
+      }
+      
+      if(element.first().hasOwnProperty('ontouchstart')) {
+        
+        var touchMove;
+        
+        element.first().addEventListener('touchstart', function () {
+          touchMove = false;
+          
+          var ink = getInk();
+          
+          // cancel if the user is performing a swiping gesture
+          setTimeout(function() {
+            if(!touchMove) {
+              showInk(ink);
+            }
+          }, 100);
+        }, true);
+        
+        element.first().addEventListener('touchmove', function () {
+          touchMove = true;
+        }, true);
+      }
+      
+      else {
+        element.on('mousedown', function () {
+          showInk(getInk());
+        });
+      }
     }
   };
 }])
+
+.directive('scroll', function () {
+  return {
+    link: function (scope, element) {
+      // I'm a little puzzled but attaching to the touchstart event
+      // allows the ripple directive to work properly within a scroll
+      // element on my iPhone 4
+      element.on('touchstart', angular.noop);
+    }
+  };
+})
 
 .directive('button', ['rippleDirective', function (rippleDirective) {
   var ripple = rippleDirective.first();

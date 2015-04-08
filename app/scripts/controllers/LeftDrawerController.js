b42ea437a09c925d9ff4409af7a912c73f293627
Daniel Nagy <Danny@Dannys-MacBook-Pro.local>
2015-04-08 14:12:37 -0400

    moving listener inside promise to be sure drawer is difined

--- app/scripts/controllers/LeftDrawerController.js
+++ app/scripts/controllers/LeftDrawerController.js
@@ -3,16 +3,17 @@
 angular.module('me').controller('LeftDrawerController', ['$drawer', '$media', '$scope', '$timeout', function ($drawer, $media, $scope, $timeout) {
   
   var drawer;
-  var mediaQuery = $media('lt-sm');
   
   $drawer.get('left').then(function (result) {
     drawer = result;
-  });
-  
-  $scope.$on('$routeChangeSuccess', function () {
-    if(mediaQuery.matches) {
-      $timeout(drawer.close, 150);
-    }
+    
+    var mediaQuery = $media('lt-sm');
+    
+    $scope.$on('$routeChangeSuccess', function () {
+      if(mediaQuery.matches) {
+        $timeout(drawer.close, 150);
+      }
+    });
   });
   
 }]);

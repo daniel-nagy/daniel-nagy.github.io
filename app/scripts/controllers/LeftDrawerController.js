3aa6119d9ef3ab9fd6ba374394aff33adfd08d6c
Daniel Nagy <Danny@Dannys-MacBook-Pro.local>
2015-04-09 21:23:22 -0400

    due to popular demand the left drawer always closed itself

--- app/scripts/controllers/LeftDrawerController.js
+++ app/scripts/controllers/LeftDrawerController.js
@@ -1,18 +1,14 @@
 'use strict';
 
-angular.module('me').controller('LeftDrawerController', ['$drawer', '$media', '$scope', '$timeout', function ($drawer, $media, $scope, $timeout) {
+angular.module('me').controller('LeftDrawerController', ['$drawer', '$scope', '$timeout', function ($drawer, $scope, $timeout) {
   
   var drawer;
   
   $drawer.get('left').then(function (result) {
     drawer = result;
     
-    var mediaQuery = $media('lt-sm');
-    
     $scope.$on('$routeChangeSuccess', function () {
-      if(mediaQuery.matches) {
-        $timeout(drawer.close, 150);
-      }
+      $timeout(drawer.close, 150);
     });
   });
   

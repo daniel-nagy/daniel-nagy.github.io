d948817db9089fd71a816e55aec8cc3cc32a7a9d
Daniel Nagy <Danny@Dannys-MacBook-Pro.local>
2015-04-12 21:35:27 -0400

    updating production

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,7 +90,7 @@ module.exports = function(grunt) {
           expand: true,
           flatten: true,
           cwd: 'app/media/',
-          src: ['*'],
+          src: ['**/*'],
           dest: 'production/media/'
         }]
       }

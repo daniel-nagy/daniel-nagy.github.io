32be244852f1b95d3a7e333132a38d2c42b0cae9
Daniel Nagy <Danny@Dannys-MacBook-Pro.local>
2015-04-12 22:02:41 -0400

    fixing dependency injection

--- app/scripts/directives/mediaPlayer.js
+++ app/scripts/directives/mediaPlayer.js
@@ -3,90 +3,86 @@
 angular.module('me').directive('mediaPlayer', ['$album', '$audio', function ($album, $audio) {
   return {
     templateUrl: 'templates/media-player.html',
-    link: postLink,
-    controller: audioController
-  };
-  
-  function postLink(scope, element) {
-    var scroll = element.find('scroll');
-    var toolbar = scroll.previous();
-    
-    scroll.on('scroll', function () {
-      if(scroll.prop('scrollTop') <= 0) {
-        toolbar.removeClass('elevated');
-      } else {
-        toolbar.addClass('elevated');
-      }
-    });
-  }
-  
-  function audioController($scope, $element, $attrs) {
-    
-    $scope.audio = $audio;
-    
-    $album($attrs.collectionId).then(function (album) {
-      $scope.album = album;
+    link: function postLink(scope, element) {
+      var scroll = element.find('scroll');
+      var toolbar = scroll.previous();
       
-      if(!$audio.isSet()) {
-        $audio.set($scope.album.currentTrack().title);
-      }
-      
-      $audio.on('timeupdate', function () {
-        $scope.$apply();
-      });
-      
-      $audio.on('ended', function () {
-        if($scope.album.hasNextTrack()) {
-          $scope.$apply(function () {
-            $audio.set($scope.album.nextTrack().title);
-            $audio.play();
-          });
+      scroll.on('scroll', function () {
+        if(scroll.prop('scrollTop') <= 0) {
+          toolbar.removeClass('elevated');
+        } else {
+          toolbar.addClass('elevated');
         }
       });
-    });
-    
-    $scope.selectTrack = function (track, index) {
-      $scope.album.selectTrack(index);
-      $audio.set(track.title);
-      $audio.play();
-    };
-    
-    $scope.selectNext = function () {
-      var playing = $audio.isPlaying();
+    },
+    controller: function audioController($scope, $element, $attrs) {
       
-      $audio.set($scope.album.nextTrack().title);
+      $scope.audio = $audio;
       
-      if(playing) {
+      $album($attrs.collectionId).then(function (album) {
+        $scope.album = album;
+        
+        if(!$audio.isSet()) {
+          $audio.set($scope.album.currentTrack().title);
+        }
+        
+        $audio.on('timeupdate', function () {
+          $scope.$apply();
+        });
+        
+        $audio.on('ended', function () {
+          if($scope.album.hasNextTrack()) {
+            $scope.$apply(function () {
+              $audio.set($scope.album.nextTrack().title);
+              $audio.play();
+            });
+          }
+        });
+      });
+      
+      $scope.selectTrack = function (track, index) {
+        $scope.album.selectTrack(index);
+        $audio.set(track.title);
         $audio.play();
-      }
-    };
-    
-    $scope.selectPrevious = function () {
-      var playing = $audio.isPlaying();
+      };
       
-      $audio.set($scope.album.previousTrack().title);
+      $scope.selectNext = function () {
+        var playing = $audio.isPlaying();
+        
+        $audio.set($scope.album.nextTrack().title);
+        
+        if(playing) {
+          $audio.play();
+        }
+      };
       
-      if(playing) {
-        $audio.play();
-      }
-    };
-    
-    $scope.toggleRepeat = function () {
-      if($audio.repeatEnabled()) {
-        $audio.repeatOff();
-      } else {
-        $audio.repeatOn();
-      }
-    };
-    
-    $scope.toggleShuffle = function () {
-      if($scope.album.isShuffled()) {
-        $scope.album.unShuffle();
-      } else {
-        $scope.album.shuffle();
-      }
-    };
-  }
+      $scope.selectPrevious = function () {
+        var playing = $audio.isPlaying();
+        
+        $audio.set($scope.album.previousTrack().title);
+        
+        if(playing) {
+          $audio.play();
+        }
+      };
+      
+      $scope.toggleRepeat = function () {
+        if($audio.repeatEnabled()) {
+          $audio.repeatOff();
+        } else {
+          $audio.repeatOn();
+        }
+      };
+      
+      $scope.toggleShuffle = function () {
+        if($scope.album.isShuffled()) {
+          $scope.album.unShuffle();
+        } else {
+          $scope.album.shuffle();
+        }
+      };
+    }
+  };
 }])
 
 .filter('zeroPad', function () {
